2d56395ef35cdc476c4bf5fe025f540a385fd7f5
cadman77 <cadman77@yandex.ru>
2022-03-26 12:17:28 +0300

    Add project files.

---
+++ Revit_API_5_3/Main.cs
@@ -0,0 +1,48 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_API_5_3
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            string tabName = "ITMO Revit-API";
+            application.CreateRibbonTab(tabName);
+
+            string panelName = "Тема 5";
+            RibbonPanel myPanel = application.CreateRibbonPanel(tabName, panelName);
+
+            string u51 = "Revit_API_5_1";
+            //string u52 = "Revit_API_5_2";
+            string utilsFolder = @"C:\ProgramData\RevitAPITraining\";
+
+            string assy_51_Path = Path.Combine(utilsFolder, $"{u51}\\", $"{u51}.dll");
+            //string assy_52_Path = Path.Combine(utilsFolder, $"{u52}\\", $"{u52}.dll");
+
+            //TaskDialog.Show("1", assy_51_Path+$"{Environment.NewLine}"+assy_52_Path);
+
+            PushButtonData pbd_5_1 = new PushButtonData("button_5_1", "Статистика (5.1)", assy_51_Path, $"{u51}.Main");
+            //PushButtonData pbd_5_2 = new PushButtonData("button_5_2", "Тип стен (5.2)", assy_52_Path, $"{u52}.Main");
+
+            //myPanel.AddStackedItems(pbd_5_1, pbd_5_2);
+            myPanel.AddItem(pbd_5_1);
+
+            return Result.Succeeded;
+        }
+    }
+}
