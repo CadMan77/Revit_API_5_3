d732f2c7f81b19236ebf73d617f305bc663959e3
cadman77 <cadman77@yandex.ru>
2022-03-26 15:41:25 +0300

    исправлены assembly пути

--- Revit_API_5_3/Main.cs
+++ Revit_API_5_3/Main.cs
@@ -28,19 +28,19 @@ namespace Revit_API_5_3
             RibbonPanel myPanel = application.CreateRibbonPanel(tabName, panelName);
 
             string u51 = "Revit_API_5_1";
-            //string u52 = "Revit_API_5_2";
+            string u52 = "Revit_API_5_2";
             string utilsFolder = @"C:\ProgramData\RevitAPITraining\";
 
-            string assy_51_Path = Path.Combine(utilsFolder, $"{u51}\\", $"{u51}.dll");
-            //string assy_52_Path = Path.Combine(utilsFolder, $"{u52}\\", $"{u52}.dll");
+            string assy_51_Path = Path.Combine(utilsFolder, $"{u51}.dll");
+            string assy_52_Path = Path.Combine(utilsFolder, $"{u52}.dll");
 
             //TaskDialog.Show("1", assy_51_Path+$"{Environment.NewLine}"+assy_52_Path);
 
             PushButtonData pbd_5_1 = new PushButtonData("button_5_1", "Статистика (5.1)", assy_51_Path, $"{u51}.Main");
-            //PushButtonData pbd_5_2 = new PushButtonData("button_5_2", "Тип стен (5.2)", assy_52_Path, $"{u52}.Main");
+            PushButtonData pbd_5_2 = new PushButtonData("button_5_2", "Тип стен (5.2)", assy_52_Path, $"{u52}.Main");
 
-            //myPanel.AddStackedItems(pbd_5_1, pbd_5_2);
             myPanel.AddItem(pbd_5_1);
+            myPanel.AddStackedItems(pbd_5_1, pbd_5_2);
 
             return Result.Succeeded;
         }
